042522665e020dcb60704f62d4d63d03a8ba47ed
Noé Henchoz <nhenchoz122@icloud.com>
2024-06-14 14:46:50 +0200

    Update bot

--- index.js
+++ index.js
@@ -30,7 +30,15 @@ function generateNewREADME() {
 }
 
 function getTodayDate() {
-  return today.toLocaleDateString('en');
+  const today = new Date();
+  return today.toLocaleString('en', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
 }
 
 function getDBNWSentence() {
