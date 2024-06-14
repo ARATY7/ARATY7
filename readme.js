fd3b4f67ab77709c5e8bb19c8648760cf202b475
Noé Henchoz <nhenchoz122@icloud.com>
2024-06-14 13:58:11 +0200

    Add bot

---
+++ readme.js
@@ -0,0 +1,31 @@
+module.exports = `
+## 😏 About Me
+
+* My name is Noé Henchoz and I'm 19 years old.
+* I just finished my final year of my IT apprenticeship. 💻
+* I’m learning Web full-stack development with Next.js, React.js, TailwindCSS, Express.js and TypeScript.
+
+---
+
+## 💻 Skills
+
+[![My Skills](https://skillicons.dev/icons?i=next,react,ts,express,php,java,tailwind,idea&theme=light)](https://skillicons.dev)
+
+---
+
+## 📊 GitHub Stats
+
+[![My Stats](https://github-readme-stats.vercel.app/api/top-langs/?username=henchoznoe&theme=vue-dark&hide_border=true&include_all_commits=true&count_private=true&langs_count=4)](https://visitcount.itsvg.in)
+
+---
+
+## 🌐 Socials
+
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?logo=Instagram&logoColor=white)](https://instagram.com/henchoznoe) [![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://linkedin.com/in/henchoznoe)
+
+---
+
+Last update on <#today_date>
+
+<#day_before_new_years> !!
+`;
